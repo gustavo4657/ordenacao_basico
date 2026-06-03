8ca2786937402479bbbbb8c7e81441503985c952
Gustavo <gustavoo.avelino@aluno.ufsj.edu.br>
2026-06-03 16:37:21 -0300

    Add insertion sort and selection sort functions
    Implemented insertion sort and selection sort algorithms.

--- ordenacao.c
+++ ordenacao.c
@@ -2,8 +2,28 @@
 
 void insertion_sort(int *v, int n){
     // Insira a lógica do algoritmo de ordenação por inserção aqui.
+    for(int i = 1; i < n; i++){
+        int chave = v[i];
+        int j = i - 1;
+        while(j >= 0 && v[j] > chave){
+            v[j + 1] = v[j];
+            j--;
+        }
+        v[j + 1] = chave;
+    }
 }
 
 void selection_sort(int *v, int n){
     // Insira a lógica do algoritmo de seleção por inserção aqui.
+    for(int i = 0; i < n - 1; i++){
+        int menor = i;
+        for(int j = i + 1; j < n; j++){
+            if(v[j] < v[menor]){
+                menor = j;
+            }
+        }
+        int temp = v[i];
+        v[i] = v[menor];
+        v[menor] = temp;
+    }
 }
